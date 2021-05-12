4d272bd973f2eb2a2117db704e9795ceb4297ba5
Koji Hisano <hisano@gmail.com>
2021-05-13 05:26:50 +0900

    wasmer-javaライブラリを追加して動作を確認

---
+++ jar/install.bat
@@ -0,0 +1,2 @@
+call mvn install:install-file -DgroupId=jp.hisano -DartifactId=wasmer-windows -Dversion=0.3.0 -Dfile=wasmer-windows-0.3.0.jar -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -DgroupId=jp.hisano -DartifactId=wasmer-windows -Dversion=0.3.0 -Dfile=wasmer-windows-0.3.0-sources.jar -Dclassifier=sources -Dpackaging=jar
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -14,6 +14,7 @@ plugins {
 repositories {
     // Use Maven Central for resolving dependencies.
     mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
@@ -22,6 +23,7 @@ dependencies {
 
     // This dependency is exported to consumers, that is to say found on their compile classpath.
     api("org.apache.commons:commons-math3:3.6.1")
+    api("jp.hisano:wasmer-windows:0.3.0")
 
     // This dependency is used internally, and not exposed to consumers on their own compile classpath.
     implementation("com.google.guava:guava:30.0-jre")
