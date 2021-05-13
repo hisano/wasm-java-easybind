a46e450b14b627095b12191486708ac0efbd5cf6
Koji Hisano <hisano@gmail.com>
2021-05-13 17:23:29 +0900

    emccのバージョンを固定化

--- lib/src/test/resources/build.bat
+++ lib/src/test/resources/build.bat
@@ -1,2 +1,2 @@
-docker run -it --rm -v %cd%:/src emscripten/emsdk emcc --no-entry -s EXPORTED_FUNCTIONS="['_hello', '_malloc', '_free']" -s WASM=1 -o hello.wasm hello.c
-docker run -it --rm -v %cd%:/src emscripten/emsdk emcc --no-entry -s EXPORTED_FUNCTIONS="['_add', '_malloc', '_free']" -s WASM=1 -o add.wasm add.c
+docker run -it --rm -v %cd%:/src emscripten/emsdk:2.0.20 emcc --no-entry -s EXPORTED_FUNCTIONS="['_hello', '_malloc', '_free']" -s WASM=1 -o hello.wasm hello.c
+docker run -it --rm -v %cd%:/src emscripten/emsdk:2.0.20 emcc --no-entry -s EXPORTED_FUNCTIONS="['_add', '_malloc', '_free']" -s WASM=1 -o add.wasm add.c
