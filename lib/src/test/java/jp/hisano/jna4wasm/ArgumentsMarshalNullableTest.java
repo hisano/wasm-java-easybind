69cce8c3dcd9010c0d8c154f81de8817f09ddd84
Koji Hisano <hisano@gmail.com>
2021-05-14 03:50:47 +0900

    ArgumentsMarshalNullableTestテストにリソース解放処理を追加

--- lib/src/test/java/jp/hisano/jna4wasm/ArgumentsMarshalNullableTest.java
+++ lib/src/test/java/jp/hisano/jna4wasm/ArgumentsMarshalNullableTest.java
@@ -201,6 +201,7 @@ public class ArgumentsMarshalNullableTest extends TestCase {
 
     @Override
     protected void tearDown() {
+        LibraryContext.get().dispose();
         lib = null;
     }
 
