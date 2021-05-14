46281cfd59b3bb1d34b80da8c5d71b4a285d5fa9
Koji Hisano <hisano@gmail.com>
2021-05-15 05:25:35 +0900

    ライブラリの依存関係を整理

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,19 +7,15 @@ repositories {
 }
 
 dependencies {
+    api("io.github.kawamuray.wasmtime:wasmtime-java:0.4.0")
+
     testImplementation(platform("org.junit:junit-bom:5.7.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.11.1")
-
     testImplementation("junit:junit:4.13.2")
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine")
 
-    compileOnly("org.projectlombok:lombok:1.18.12")
-
-    api("org.apache.commons:commons-math3:3.6.1")
-    api("io.github.kawamuray.wasmtime:wasmtime-java:0.4.0")
-
-    implementation("com.google.guava:guava:30.0-jre")
+    testImplementation("org.assertj:assertj-core:3.11.1")
+    testImplementation("com.google.guava:guava:30.0-jre")
 }
 
 tasks.test {
